d194a74b2295ab1a7522455fee8ba7fc2aa1e937
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-03-03 08:27:51 +0530

    add file

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,10 +1,23 @@
 package com.driver;
 import java.util.Random;
+
 public class BankAccount {
 
     private String name;
-    double balance;
+    private double balance;
     private double minBalance;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
 
     public BankAccount(String name, double balance, double minBalance) {
         this.name = name;
@@ -16,35 +29,51 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        Random random = new Random();
-        StringBuilder accountNumber = new StringBuilder();
-        int currentSum = 0;
 
-        for (int i = 0; i < digits; i++) {
-            int digit = random.nextInt(10); // Random digit between 0 and 9 (inclusive)
-            currentSum += digit;
-            accountNumber.append(digit);
-        }
-
-        if (currentSum != sum) {
+//        remaining number
+        int rem = sum;
+        String accNo = "";
+        if(digits*9 < sum){
             throw new Exception("Account Number can not be generated");
         }
-
-        return accountNumber.toString();
-
+        else{
+            while(digits > 0 && rem > 0){
+                if(rem >= 9){
+                    rem = rem - 9;
+                    accNo = accNo + "9";
+                }
+                else{
+                    accNo = accNo + Integer.toString(rem);
+                    rem = 0;
+                }
+                digits--;
+            }
+            while(digits > 0){
+                accNo = accNo + "0";
+                digits--;
+            }
+            return accNo;
+        }
+//        return null;
     }
 
     public void deposit(double amount) {
         //add amount to balance
-        balance += amount;
+
+        balance = balance + amount;
+
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-        if (balance - amount < minBalance) {
+
+        if(this.balance-amount>=getMinBalance())
+        {
+            balance=balance-amount;
+        }
+        else {
             throw new Exception("Insufficient Balance");
         }
-        balance -= amount;
     }
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -4,31 +4,51 @@ public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
 
+    public double getRate() {
+        return rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
-        super(name, balance, 0); // minimum balance is 0 by default
-        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        super(name,balance,0);
         this.rate = rate;
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
 
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-        if (amount > maxWithdrawalLimit) {
-            throw new Exception("Maximum Withdraw Limit Exceed");
+        if(amount <= maxWithdrawalLimit) {
+            super.withdraw(amount);
         }
-        super.withdraw(amount);
+        else {
+            throw new Exception("Maximum Withdraw Limit Exceeded");
+        }
+
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-        return balance * rate * years;
+//        double principle = getBalance();
+//        double finalAmount = (principle*rate*years)/100;
+//        return finalAmount;
+        return getBalance()*(1 + (rate*years)/100);
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-        return balance * Math.pow(1 + rate / times, times * years) - balance;
+//        double principle = getBalance();
+//        double finalAmount = principle * Math.pow((1 + (rate/times)), times*years);
+//        return finalAmount;
+
+        double compoundInterest = getBalance() * Math.pow((1 + rate / (100*times)), times * years);
+        //  return compoundInterest;
+        return compoundInterest;
     }
 
 }
